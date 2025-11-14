b735fbc7acda2a9f7d5dd1dabc97bff9467492de
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 14:35:41 -0300

    Criação do model de pedido

---
+++ Models/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5.Classes
+{
+    internal class Pedido
+    {
+        private int idPedido;
+        private DateOnly estimativaEntrega;
+        private string formaPagamento;
+        public Pedido(Cliente cliente, int id, DateOnly estimativa, string formaPagamento) { }
+
+        public DateOnly EstimativaEntrega { get; set; }
+        public string FormaPagamento { get; set; }
+    }
+}
