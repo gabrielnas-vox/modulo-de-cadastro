ce1ca15f7c7be7ae9fcb7035e1529f7335917883
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 14:37:33 -0300

    Inserção da classe main já com cadastro de um cliente

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Fase5.Classes;
+using Fase5.Services;
+
+FuncoesCliente funcoesCliente = new FuncoesCliente();
+
+Cliente novoCliente = funcoesCliente.CadastrarCliente("Thiago", "Rua teste, 123", "312");
+
+Console.WriteLine(novoCliente.Endereco);
