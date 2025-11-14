94011525e0be0aa31abb64fc9ba0e719bf5300b9
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 15:11:35 -0300

    Inserido método construtor model Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,11 +8,20 @@ namespace Fase5.Classes
 {
     internal class Produto
     {
-        private string idProduto;
+        private int idProduto;
         private string nomeProduto;
         private int quantidadeEstoque;
         private string categoria;
         private string descricao;
+
+        public Produto(int idProduto, string nomeProduto, int quantidadeEstoque, string categoria, string descricao)
+        {
+            this.idProduto = idProduto;
+            this.NomeProduto = nomeProduto;
+            this.QuantidadeEstoque = quantidadeEstoque;
+            this.Categoria = categoria;
+            this.Descricao = descricao;
+        }
 
         public string NomeProduto { get; set; }
         public int QuantidadeEstoque { get; set; }
