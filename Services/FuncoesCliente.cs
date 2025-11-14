40f5ef2a77baa644b3d5084b8094c4e30a52e39b
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 14:36:37 -0300

    Criado o service do model de cliente

---
+++ Services/FuncoesCliente.cs
@@ -0,0 +1,26 @@
+﻿using Fase5.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5.Services
+{
+    internal class FuncoesCliente
+    {
+        private int idAtual = 0;
+
+        public Cliente CadastrarCliente(string nome, string endereco, string cpf)
+        {
+            Cliente novoCliente = new Cliente(
+                idAtual++,
+                nome,
+                endereco,
+                cpf
+            );
+
+            return novoCliente;
+        }
+    }
+}
