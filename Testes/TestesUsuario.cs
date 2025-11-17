f0a0d916a18a9218f7a2758a3175e56fdff9d05a
gabrielnas-vox <gabriel.nascimento@voxsolucoes.com.br>
2025-11-17 15:59:56 -0300

    Teste sucesso e falho de cadastro de usuário

--- Testes/TestesUsuario.cs
+++ Testes/TestesUsuario.cs
@@ -6,15 +6,15 @@ using Assert = Xunit.Assert;
 
 namespace Fase5.Testes
 {
-    internal class TestesUsuario
+    public class TestesUsuario
     {
         FuncoesUsuario funcoes = new FuncoesUsuario();
 
-        public List<Usuario> ListaUsuariosFakes()
+        public List<Usuario> ListaUsuariosFakesCertos()
         {
             var usuarioFaker = new Faker<Usuario>("pt_BR")
                 .RuleFor(c => c.Email, f => f.Internet.Email(f.Person.FirstName))
-                .RuleFor(c => c.Senha, f => f.Person.LastName)
+                .RuleFor(c => c.Senha, f => f.Random.Number(100000000, 2000000000).ToString())
                 .RuleFor(c => c.Username, f => f.Person.FirstName);
 
             var usuarios = usuarioFaker.Generate(10);
@@ -24,14 +24,51 @@ namespace Fase5.Testes
             return usuarios;
         }
 
-        [Fact]
-        public void testarCadastro()
+        public List<Usuario> ListaUsuariosFakesErrados()
         {
-            List<Usuario> lista = ListaUsuariosFakes();
+            var usuarioFaker = new Faker<Usuario>("pt_BR")
+                .RuleFor(c => c.Email, f => f.Internet.Email(f.Person.FirstName).OrNull(f, .1f))
+                .RuleFor(c => c.Senha, f => f.Random.Number(1000000, 20000000).ToString().OrNull(f, .1f))
+                .RuleFor(c => c.Username, f => f.Person.FirstName.OrNull(f, .1f));
+
+            var usuarios = usuarioFaker.Generate(10);
+
+            usuarios.ForEach(u => Console.WriteLine(u.Email));
+
+            return usuarios;
+        }
+
+        [Fact]
+        public void testarCadastroSucesso()
+        {
+            List<Usuario> lista = ListaUsuariosFakesCertos();
 
             foreach (var usuario in lista)
             {
                 Assert.NotNull(usuario.Email);
+
+                Assert.NotNull(usuario.Senha);
+                Assert.False(usuario.Senha.Count() < 8, $"A senha precisa ser maior que 8 caracteres");
+
+                Assert.NotNull(usuario.Username);
+            }
+        }
+
+        [Fact]
+        public void testarCadastroFalho()
+        {
+            List<Usuario> lista = ListaUsuariosFakesErrados();
+
+            foreach (var usuario in lista)
+            {
+                Assert.NotNull(usuario.Email);
+
+                Assert.NotNull(usuario.Senha);
+                Assert.False(usuario.Senha.Count() < 8, $"A senha precisa ser maior que 8 caracteres");
+
+                Assert.NotNull(usuario.Username);
+
+                Console.WriteLine($"Senha: {usuario.Senha}");
             }
         }
     }
